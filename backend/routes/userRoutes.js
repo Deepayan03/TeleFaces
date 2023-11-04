8d5da357edfcdbbc24a9a86d9338d88114eb0fe0
Deepayan03 <121600646+rider03@users.noreply.github.com>
2023-11-05 00:09:58 +0530

    Defined the multer middleware for uploading files to the cloud

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,15 +10,16 @@ import {
   resetPassword,
   updateUser,
 } from "../controllers/userControllers.js";
+import upload from "../middlewares/multerMiddleware.js";
 
 const userRouter = Router();
 // Defining user routes
-controller.post("/register", register);
+controller.post("/register",upload.single("avatar"), register);
 controller.post("/login", login);
 controller.get("/logout", logout);
 controller.get("/getProfile/", isLoggedIn, getProfile);
 controller.post("/forgotPassword", forgotPassword);
 controller.post("/resetPassword/:resetToken", resetPassword);
 controller.post("/changePassword", isLoggedIn, changePassword);
-controller.put("/update", isLoggedIn, updateUser);
+controller.put("/update", isLoggedIn,upload.single("avatar"),  updateUser);
 export default userRouter;
